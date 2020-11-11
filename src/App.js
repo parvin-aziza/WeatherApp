ee8168bda965b18b031857361dff66b7afbebd5d
Aziza <azizaparvin07@gmail.com>
2020-11-11 21:43:30 +0530

    Added Date and Time

--- src/App.js
+++ src/App.js
@@ -6,11 +6,31 @@ const api ={
 }
 
 function App() {
-  return (
+
+  const dateBuilder = (d) => {
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let day = days[d.getDay()]
+    let date = d.getDate();
+    let month=months[d.getMonth()];
+    let year=d.getFullYear();
+
+    return`${day} ${date} ${month} ${year}`
+  }
+    return (
     <div className="app">
      <main>
        <div className="search-box">
          <input type="text" className="search-bar" placeholder="Search...."></input>
+       </div>
+       <div className="location-box">
+         <div className="location">Bangalore,India</div>
+         <div className="date">{dateBuilder(new Date())}</div>
+         <div className="weather-box">
+           <div className="temp">15*c</div>
+           <div className="weather">Sunny</div>
+         </div>
+
        </div>
      </main>
     </div>
