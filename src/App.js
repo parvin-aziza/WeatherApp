e3be63719e141319a9b324eae6e294a234dd29b6
Aziza <azizaparvin07@gmail.com>
2020-11-17 13:03:04 +0530

    Final code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 const api ={
   key: "4cd4590a9f682f3d4c1a9b175e9380d3",
@@ -6,7 +6,20 @@ const api ={
 }
 
 function App() {
+  const [query,setQuery] = useState('');
+  const [weather, setWeather] =useState({});
 
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
   const dateBuilder = (d) => {
     let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
@@ -17,22 +30,34 @@ function App() {
 
     return`${day} ${date} ${month} ${year}`
   }
-    return (
-    <div className="app">
-     <main>
-       <div className="search-box">
-         <input type="text" className="search-bar" placeholder="Search...."></input>
-       </div>
-       <div className="location-box">
-         <div className="location">Bangalore,India</div>
-         <div className="date">{dateBuilder(new Date())}</div>
-         <div className="weather-box">
-           <div className="temp">15*c</div>
-           <div className="weather">Sunny</div>
-         </div>
-
-       </div>
-     </main>
+  return (
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°c
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
+          </div>
+        </div>
+        ) : ('')}
+      </main>
     </div>
   );
 }
